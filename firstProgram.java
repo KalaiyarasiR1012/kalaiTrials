81379d52271546764a2f972cccddbfefcdcb3799
Kalaiyarasi <Kalaiarasi1091@gmail.com>
2020-08-02 20:05:52 +0530

    My second change in firstprogram

--- firstProgram.java
+++ firstProgram.java
@@ -2,7 +2,9 @@ public program{
 
 public static void main(sting [] args){
 
-system.out.println("My first program")
+system.out.println("My first commit print message");
+system.out.println("My second commit print message");
+
 
 }
 }
